2afa29f1570b2918a09a9aeac62d69bc9fb7be98
Masha <olkhovkayamasha@gmail.com>
2023-06-23 19:14:40 +0100

    created bricks and centered canvas

--- index.js
+++
@@ -1,3 +0,0 @@
-function setup() { createCanvas(600, 400); }
-
-function draw() { background(220); }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
-function setup() { createCanvas(600, 400); }
+let brickHeight = 40;
+let roadPos = 0;
 
-function draw() { background(220); }
+function setup() {
+    const canvas = createCanvas(600, 400);
+    canvas.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2);
+}
+
+function draw() {
+  background(100);
+  
+  // draw brick road
+  fill(153, 77, 0);
+  for (let i = 0; i < width / 40; i++) {
+    rect(i * 40 - roadPos, height - brickHeight, 40, brickHeight);
+  }
+  
+  // move the road
+  roadPos += 2;
+  if (roadPos > 40) {
+    roadPos = 0;
+  }
+  
+  // draw additional shapes or elements
+  fill(255, 0, 0);
+  // Add code here to draw other objects on the screen
+  
+}
