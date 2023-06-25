c5912a4d271118f0470d4a4cfc99a7fdb81681aa
Masha <olkhovkayamasha@gmail.com>
2023-06-25 11:22:40 +0100

    trees evenly spaced out

--- js/index.js
+++ js/index.js
@@ -16,67 +16,81 @@ let characterImages = [];
 let currentImageIndex = 0;
 
 function preload() {
+    //pushes character images into an array
     characterImages.push(loadImage("assets/female_walk1.png"));
     characterImages.push(loadImage("assets/female_walk2.png"));
 
+    //pushes tree images into an array
     treeImages.push(loadImage("assets/tree1.png"));
     treeImages.push(loadImage("assets/tree2.png"));
     treeImages.push(loadImage("assets/tree3.png"));
     treeImages.push(loadImage("assets/tree4.png"));
 }
-
+//creating tree class
 class Tree {
     constructor(x) {
         this.width = 30;
+        //height of tree variable
         this.height = random(140, 260);
         this.x = x;
+        //bottom of tree starts from the bricks
         this.y = height - brickHeight - this.height;
         this.image = random(treeImages);
     }
-
+    //dispalys tree
     show() {
         image(this.image, this.x, this.y, this.width, this.height);
     }
-
+    //changes the x-position of tree in line with road speed
     move() {
         this.x -= roadSpeed;
     }
 }
 
 function setup() {
-    //createCanvas(600, 400);
+    //centred canvas
     const canvas = createCanvas(600, 400);
     canvas.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2);
+    //space between trees so 4 fit in at a time, evenly spaced out
     let treeSpacing = width / 4;
+    //constructs and pushes tree objects into an array tree, with treespacing being,
+    // with even spacing
     for (let i = 0; i < 4; i++) {
         trees.push(new Tree(treeSpacing * i));
     }
-
+    //every 100 millisecods calls the updateCharacterPosition function to change
+    //the walking images of the character 
     setInterval(updateCharacterPostition, 100);
 }
 
 function draw() {
     background(220);
-
+//in response to keypressed the character postition changes the x and y position, respectively
+//validates that character  doesn't go off screen to the left (stays half a character)
     if (leftPressed && charX > charSize / 2) {
         charX -= charSpeed;
     }
+    //validates that charX doesn't go off screen to the right
     if (rightPressed && charX < width - charSize / 2) {
         charX += charSpeed;
     }
+    //validates that character doesn't go off screen upwards
     if (upPressed && charY > charSize / 2) {
         charY -= charSpeed;
     }
+    //validates that charY doesn't go below the brick height
     if (downPressed && charY < height - brickHeight - charSize / 2) {
         charY += charSpeed;
     }
-
+//created blocks to fit on screen with current roadPos
     fill(153, 77, 0);
-    for (let i = 0; i < width / 40; i++) {
-        rect(i * 40 - roadPos, height - brickHeight, 40, brickHeight);
+    for (let i = 0; i < width / 10; i++) {
+        rect(i * 10 - roadPos, height - brickHeight, 10, brickHeight);
     }
+    //updates roadPos after creating blocks
     roadPos += 2;
-    if (roadPos > 40) {
+    //validates that only 40 blocks appear at a time, otherwise resets to 0 to start 
+    if (roadPos > 10) {
         roadPos = 0;
     }
 
